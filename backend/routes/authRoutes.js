9d6f1b885c11b85b86ebffad6eda12eb2a850203
jahidul96 <jahidul59895@gmail.com>
2023-01-23 11:53:11 +0600

    pass hash and some error solvedgit add .

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,14 +1,27 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
 // model imports
 const User = require("../models/userModel");
 
 router.post("/register", async (req, res) => {
+  const { email } = req.body;
+  const salt = 10;
+  const hasedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hasedPassword;
   try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(403).json({
+        message: "User already Exists",
+      });
+    }
+
     const newUser = new User(req.body);
+    const user = await newUser.save();
     res.json({
       message: "user created",
-      user: newUser,
+      user,
     });
   } catch (error) {
     res.json({
